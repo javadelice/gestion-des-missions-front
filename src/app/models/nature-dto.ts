ed2be1e8745bb4af3c86247aae9c7eb9dbcf0068
Clement BRIEUC <clementbrieuc@gmail.com>
2019-08-23 17:56:36 +0200

    ajout colonnes plafondFrais et Dépt. frais

--- src/app/models/nature-dto.ts
+++ src/app/models/nature-dto.ts
@@ -1,4 +1,4 @@
 export class NatureDto {
 
-  constructor(public code: string, public isFacturee:string, public hasPrime:string, public tjm:number, public pourcentagePrime:number, public depassPlafond:string, public debutValidite:string, public finValidite:string) {}
+  constructor(public code: string, public isFacturee:string, public hasPrime:string, public tjm:number, public pourcentagePrime:number, public plafondFrais:number, public depassPlafond:string, public debutValidite:string, public finValidite:string) {}
 }
