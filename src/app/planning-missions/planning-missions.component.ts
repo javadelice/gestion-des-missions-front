e608d3d2b68ede170c13eccbb4f0f9ae4a111c54
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-08-27 18:44:11 +0200

    issue-10 : ajout fonctionnalité cliquer sur mission du planning

--- src/app/planning-missions/planning-missions.component.ts
+++ src/app/planning-missions/planning-missions.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import * as moment from 'moment';
 import { generate } from 'rxjs';
 import { MissionDto } from '../models/mission-dto';
 import { MissionsService } from '../missions/missions.service';
 import { AuthService } from '../auth/auth.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { PlanningService } from './planning.service';
 
 @Component({
   selector: 'app-planning-missions',
@@ -19,15 +21,19 @@ export class PlanningMissionsComponent implements OnInit {
   datesInMonth = [];
   weeksInMonth: number[][];
 
+  mission: MissionDto;
   missions: MissionDto[];
   missionDates: {
     date: number;
     type: string;
+    missionId: number;
   }[];
   dateTemp;
   isError: boolean;
 
-  constructor(private missionService: MissionsService, private _authService: AuthService) { }
+  modalRef: BsModalRef;
+
+  constructor(private planningService: PlanningService, private missionService: MissionsService, private _authService: AuthService, private modalService: BsModalService) { }
 
   ngOnInit() {
     this._authService.collegueConnecteObs.subscribe(collegueConnecte => {
@@ -67,7 +73,7 @@ export class PlanningMissionsComponent implements OnInit {
         this.dateTemp = moment(mission.startDate);
         while (moment(this.dateTemp).isBefore(this.currentDate.endOf('month')) && moment(this.dateTemp).isBefore(moment(mission.endDate).add(1, 'day'))) {
           if (moment(this.dateTemp).isoWeekday() !== 6 && moment(this.dateTemp).isoWeekday() !== 7) {
-            this.missionDates.push({date: moment(this.dateTemp).date(), type: mission.nature.code});
+            this.missionDates.push({date: moment(this.dateTemp).date(), type: mission.nature.code, missionId: mission.id});
           }
           this.dateTemp = this.dateTemp.add(1, 'day');
         }
@@ -93,6 +99,15 @@ export class PlanningMissionsComponent implements OnInit {
   nextYear(): void {
     this.currentDate = moment(this.currentDate).add(1, 'year');
     this.ngOnInit();
+  }
+
+  openModalMission(template: TemplateRef<any>, idMission: number) {
+    this.planningService.getMission(idMission).subscribe(mission => {
+      this.mission = mission;
+      this.modalRef = this.modalService.show(template);
+    }, (error: HttpErrorResponse) => {
+      this.isError = true;
+    });
   }
 
 }
--- src/app/planning-missions/planning.service.ts
+++ src/app/planning-missions/planning.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MissionDto } from '../models/mission-dto';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,8 @@ import { HttpClient } from '@angular/common/http';
 export class PlanningService {
 
   constructor(private httpClient: HttpClient) { }
+
+  getMission(idMission: number): Observable<MissionDto> {
+    return this.httpClient.get<MissionDto>(environment.baseUrl + 'mission?idMission=' + idMission, {withCredentials: true});
+  }
 }
